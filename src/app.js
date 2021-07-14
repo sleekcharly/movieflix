b7c40c80003ed0276f74897c58140ebc12f0eff5
sleekcharly <c_ukasoanya@yahoo.com>
2021-07-14 03:02:44 +0100

    created react touter routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import React from "react";
-import { FaqsContainer } from "./containers/faqs";
-import { FooterContainer } from "./containers/footer";
-import { JumbotronContainer } from "./containers/jumbotron";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+
+import { Home, Browse, Signup, Signin } from "./pages";
 
 export default function App() {
   return (
-    <React.Fragment>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </React.Fragment>
+    <Router>
+      <Route exact path={ROUTES.HOME} component={Home} />
+      <Route exact path={ROUTES.SIGN_UP} component={Signup} />
+      <Route exact path={ROUTES.SIGN_IN} component={Signin} />
+      <Route exact path={ROUTES.BROWSE} component={Browse} />
+    </Router>
   );
 }
