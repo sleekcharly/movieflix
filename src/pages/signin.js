41fc726150f28be52f44e61f3869c6fd3620d1fa
sleekcharly <c_ukasoanya@yahoo.com>
2021-07-25 01:56:37 +0100

    created signin page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,79 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
+import { FooterContainer } from "../containers/footer";
+import { HeaderContainer } from "../containers/header";
+import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
 export default function Signin() {
-  return <p>Hello from the sign in!</p>;
+  const history = useHistory();
+  // destructure firebase from the firebase context
+  const { firebase } = useContext(FirebaseContext);
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInValid = password === "" || emailAddress === "";
+
+  // handle the signin process
+  const handleSignin = event => {
+    event.preventDefault();
+
+    // firebase work here!
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        //push to the browse page
+        history.push(ROUTES.BROWSE);
+      })
+      .catch(error => {
+        setEmailAddress("");
+        setPassword("");
+        setError(error.message);
+      });
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+
+            <Form.Input
+              type="password"
+              placeholder="Password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+
+            <Form.Submit disabled={isInValid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            New to Movieflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+
+      <FooterContainer />
+    </>
+  );
 }
